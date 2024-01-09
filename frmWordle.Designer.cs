e768fbebe44dd8d8fae885320ef546c4b89839c2
Mozsii <george.allman777@gmail.com>
2024-01-09 12:29:38 +1100

    Add project files.

---
+++ frmWordle.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Wordle_SDD
+{
+    partial class frmWordle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Rockwell", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(321, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(143, 43);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Wordle";
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.BackgroundImage = global::Wordle_SDD.Properties.Resources.imgHelpIconLight;
+            this.btnHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btnHelp.Location = new System.Drawing.Point(738, 12);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(59, 53);
+            this.btnHelp.TabIndex = 3;
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackgroundImage = global::Wordle_SDD.Properties.Resources.imgSettingsIconLight;
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btnSettings.Location = new System.Drawing.Point(12, 13);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(54, 50);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // frmWordle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(809, 821);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmWordle";
+            this.Text = "Wordle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ frmWordle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wordle_SDD
+{
+    public partial class frmWordle : Form
+    {
+        public frmWordle()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            Form frmSettings = new frmSettings();
+            frmSettings.Show();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            Form frmHelp = new frmHelp();
+            frmHelp.Show();
+        }
+    }
+}
