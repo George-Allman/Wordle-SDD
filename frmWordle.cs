c110b266c0ca5ee52f33358e0fcaa06c307c0023
Mozsii <george.allman777@gmail.com>
2024-01-09 13:00:37 +1100

    tried adding public variables

--- frmWordle.cs
+++ frmWordle.cs
@@ -12,9 +12,35 @@ namespace Wordle_SDD
 {
     public partial class frmWordle : Form
     {
+        public Color baseColor 
+        {  
+            get {  return baseColor; }
+            set {  baseColor = value; }
+        }
+        public Color alternateColor 
+        { 
+            get { return alternateColor; }
+            set { alternateColor = value; }
+        }
+        public bool darkMode 
+        { 
+            get { return darkMode; } 
+            set {  darkMode = value; } 
+        }
         public frmWordle()
         {
             InitializeComponent();
+            if (darkMode == true)
+            {
+                alternateColor = Color.FromArgb(75, 75, 75);
+                baseColor = Color.FromArgb(20, 20, 20);
+            }
+            else
+            {
+                alternateColor = Color.FromArgb(200,200,200);
+                baseColor = Color.FromArgb(245,245,245);
+            }
+            this.BackColor = baseColor;
         }
 
         private void btnSettings_Click(object sender, EventArgs e)
---
+++ letterBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wordle_SDD
+{
+    public partial class letterBox : UserControl
+    {
+        private frmWordle wordleForm = new frmWordle();
+        public letterBox()
+        {
+            InitializeComponent();
+            this.Width = 70;
+            this.Height = 70;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Color baseColor = wordleForm.baseColour;
+            Color alternateColor = wordleForm.alternateColour;
+            Font charFont = new Font("Arial", 24, FontStyle.Bold);
+
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle,
+                alternateColor, 2, ButtonBorderStyle.Solid,
+                alternateColor, 2, ButtonBorderStyle.Solid,
+                alternateColor, 2, ButtonBorderStyle.Solid,
+                alternateColor, 2, ButtonBorderStyle.Solid
+                );
+
+            BackColor = baseColor;
+
+            string letter = "X";
+            int x = (ClientSize.Width - (int)g.MeasureString(letter, charFont).Width) / 2;
+            int y = (ClientSize.Height - (int)g.MeasureString(letter, charFont).Height) / 2;
+
+            g.DrawString(letter, charFont, Brushes.White, x, y);
+        }
+    }
+}
